e82086dcac9b5704707c3cfc8b9f32da01bd7e6c
Robin Fussell <rfussell17@gmail.com>
2020-02-03 12:01:58 -0800

    styling started

---
+++ src/components/Output.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+function Output(){
+  return(
+    <h1></h1>
+  )
+}
+
+export default Output;
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-function Input(){
+function UserList(){
   return(
     <h1></h1>
   )
 }
 
-export default Input;
+export default UserList;
