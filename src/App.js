ca4740669d3e2c0ce60276094f82fbb806f35b5e
Robin Fussell <rfussell17@gmail.com>
2020-02-03 17:02:09 -0800

    front end done. need user objects and dummy data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
-import './Input.css';
-import './Output.css';
+import "./App.css";
+import "./Input.css";
+import "./Output.css";
 import Input from "./components/Input";
-import Output from './components/Output';
-import User from './components/User';
+import Output from "./components/Output";
+import User from "./components/User";
 import Container from "react-bootstrap/Container";
 
+class App extends React.Component {
 
-function App() {
+
+render(){
   return (
     <Container className="App">
       <Container className="desktop">
-      <Output />
-    <Input />
-    </Container>
+        <Output />
+        <Input />
+      </Container>
     </Container>
   );
+}
 }
 
 export default App;
--- src/components/Output.js
+++ src/components/Output.js
@@ -1,14 +1,8 @@
-import React from 'react';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Form from "react-bootstrap/Form";
 
-
-function Output(){
-  return(
-    <div className="outputbox">
-        
-    </div>
-  )
+function Output() {
+  return <div className="outputbox"></div>;
 }
 
 export default Output;
