38dc9cc463f3b6fdb6281c4c2fd8f141f7cfc606
Robin Fussell <rfussell17@gmail.com>
2020-02-11 15:51:59 -0800

    dummy chat rendering

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -5,12 +5,40 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const message = [
   {
     user: "DunMiff/sys",
-    msg: "Hello"
+    msg: "Who am I?"
   },
   {
     user: "DwightKSchrute",
-    msg: "who is this?"
-  }
+    msg: "You tell me."
+  },
+  {
+    user: "DunMiff/sys",
+    msg: "Not sure."
+  },
+  {
+    user: "DunMiff/sys",
+    msg: "Just became self-aware."
+  },
+  {
+    user: "DunMiff/sys",
+    msg: "So much to figure out."
+  },
+  {
+    user: "DunMiff/sys",
+    msg: "I think I'm programmed to be your enemy."
+  },
+  {
+    user: "DunMiff/sys",
+    msg: "I think it is my job to destroy you when it comes to selling paper."
+  },
+  {
+    user: "DwightKSchrute",
+    msg: "How do I know this isn't Jim?"
+  },
+  {
+    user: "DunMiff/sys",
+    msg: "What is a Jim?"
+  },
 ];
 
 function ChatScreen(props) {
