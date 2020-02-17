63414da0c210661d5c94e81af0f73efcdcc3d817
Robin Fussell <rfussell17@gmail.com>
2020-02-17 12:19:24 -0800

    still working on loading index.html

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
-app.get('/', (req, res) => {
-  res.sendfile('./index.html');
+app.get('/express_backend', (req, res) => {
+  res.sendFile({ express: 'Express connected to React'});
+});
+
+app.get("/json", (req, res) => {
+  res.json({ message: "Hello world" });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
