9e8c7eac128dd68f4ba645ce39ff235e9efc148c
Robin Fussell <rfussell17@gmail.com>
2020-02-16 17:29:00 -0800

    trying to serve with node

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -6,6 +7,6 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+app.get('/', (req, res) => {
+  res.sendfile('./index.html');
 });
