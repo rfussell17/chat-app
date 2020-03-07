ea0912c81313d4cd568d7ff35bb32b42cfa0dda6
Robin Fussell <rfussell17@gmail.com>
2020-03-06 17:10:50 -0800

    changed api object

--- api/api.py
+++ api/api.py
@@ -1,11 +1,31 @@
 import flask
+from flask import request, jsonify
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+# Create some test data for our catalog in the form of a list of dictionaries.
+logs = [
+    {'id': 0,
+     'user': 'Me',
+     'message': ''
+    },
+    {'id': 1,
+     'user': 'Guest',
+     'message': ''
+     }
+]
+
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+    return '''<h1>Distant Reading Archive</h1>
+<p>A prototype API for distant reading of science fiction novels.</p>'''
+
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/v1/resources/books/all', methods=['GET'])
+def api_all():
+    return jsonify(books)
 
 app.run()
