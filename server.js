638aaa8712a317282dc76e2331347749f46fa8bd
Robin Fussell <rfussell17@gmail.com>
2020-03-20 11:50:51 -0700

    working on routes

--- server.js
+++ server.js
@@ -4,11 +4,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 var path = require("path");
 
-// create a GET route
-app.get('/express_backend', (req, res) => {
-  res.sendFile({ express: 'Express connected to React'});
-});
-
 app.get("/json", (req, res) => {
   res.json({ message: "Hello world" });
 });
@@ -17,24 +12,24 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + '/client/build/index.html');
 });
 
-app.get('/api/messages', (req, res) => {
-  console.log("messages get request")
+app.get('api/messages', function (req, res) {
+  res.send('message get request')
 });
 
 app.post('api messages', (req, res) => {
-  console.log("message post request")
+  res.send('message post request')
 });
 
 app.get('api/users', (req, res) => {
- console.log("users get request")
+  res.send('user get request')
 });
 
 app.post('api/users', (req, res) => {
-  console.log("users post request")
+  res.send('user post request')
 });
 
 app.get('api/users/{id}', (req, res) => {
-  console.log("user id request")
+  res.send('user id get request')
 });
 
 
