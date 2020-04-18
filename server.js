123592fbae961e3f147430900f036e30009f81f0
Robin Fussell <rfussell17@gmail.com>
2020-04-17 18:51:39 -0700

    201 created working, need to append id to location header

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post("/api/users", (req, res) => {
     name: req.body.name,
   };
   users.push(user);
-  res.status(201).send(user);
+  res.status(201).setHeader('Location', "/api/users/:id").send(user);
 });
 
 app.post("/api/messages", (req, res) => {
